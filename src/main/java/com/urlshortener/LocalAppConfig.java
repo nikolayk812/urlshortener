e8c4953fb7c51d3dda66ceed1ca6aaa88a7e3a3d
Nikolay Kuznetsov <nkuznetsov@devexperts.com>
2016-11-14 20:51:22 +0300

    create DB from SQL file at startup

--- src/main/java/com/urlshortener/LocalAppConfig.java
+++ src/main/java/com/urlshortener/LocalAppConfig.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 
@@ -20,7 +24,9 @@ public class LocalAppConfig extends AppConfig {
 
     @Bean
     public DataSource dataSource() {
-        return createHikariDataSource(url, username, password);
+        DataSource dataSource = createHikariDataSource(url, username, password);
+        DatabasePopulatorUtils.execute(createDatabasePopulator(), dataSource);
+        return dataSource;
     }
 
     @Bean
@@ -28,4 +34,10 @@ public class LocalAppConfig extends AppConfig {
         return new UrlHitCountingCache();
     }
 
+    private DatabasePopulator createDatabasePopulator() {
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.setContinueOnError(true);
+        databasePopulator.addScript(new ClassPathResource("db/h2/initDB.sql"));
+        return databasePopulator;
+    }
 }
