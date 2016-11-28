b50661d70d2aa33b9fe30ed7d620b2c23a1f3277
Nikolay Kuznetsov <nkuznetsov@devexperts.com>
2016-11-28 19:35:11 +0300

    change SQL for pooled-lo

--- src/main/resources/db/h2/initDB.sql
+++ src/main/resources/db/h2/initDB.sql
@@ -2,33 +2,30 @@ DROP TABLE IF EXISTS url_stats;
 DROP TABLE IF EXISTS account_short_urls;
 DROP TABLE IF EXISTS short_urls;
 DROP TABLE IF EXISTS accounts;
-DROP SEQUENCE IF EXISTS global_seq;
-
-CREATE SEQUENCE global_seq START WITH 100;
 
 CREATE TABLE accounts (
-  id       INTEGER DEFAULT global_seq.nextval PRIMARY KEY,
+  id       INTEGER PRIMARY KEY,
   name     VARCHAR UNIQUE NOT NULL,
   password VARCHAR        NOT NULL
 );
 
 CREATE TABLE short_urls (
-  id               INTEGER DEFAULT global_seq.nextval PRIMARY KEY,
-  short_url        VARCHAR UNIQUE NOT NULL,
-  target_url       VARCHAR NOT NULL,
-  redirect_type    VARCHAR NOT NULL,
+  id            INTEGER PRIMARY KEY,
+  short_url     VARCHAR UNIQUE NOT NULL,
+  target_url    VARCHAR        NOT NULL,
+  redirect_type VARCHAR        NOT NULL,
   CONSTRAINT unique_target_url_redirect_type UNIQUE (target_url, redirect_type)
 );
 
 CREATE TABLE account_short_urls (
-    account_id INTEGER NOT NULL,
-    short_url_id INTEGER NOT NULL,
-    FOREIGN KEY (account_id) REFERENCES accounts (id),
-    FOREIGN KEY (short_url_id) REFERENCES short_urls (id)
+  account_id   INTEGER NOT NULL,
+  short_url_id INTEGER NOT NULL,
+  FOREIGN KEY (account_id) REFERENCES accounts (id),
+  FOREIGN KEY (short_url_id) REFERENCES short_urls (id)
 );
 
 CREATE TABLE url_stats (
-  url_id INTEGER PRIMARY KEY ,
+  url_id      INTEGER PRIMARY KEY,
   hit_counter INTEGER NOT NULL,
   FOREIGN KEY (url_id) REFERENCES short_urls (id)
 );
